09cf2a65954e6e739f25314dc13b752b5f572550
DamiJJJ <damipl1280@outlook.com>
2023-03-09 21:55:39 +0100

    Menu interactions

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,12 +9,46 @@ public class GameManager : MonoBehaviour
     public GameObject gameOver;
     private int score;
 
+    private void Awake()
+    {
+        Application.targetFrameRate = 60;
+        Pause();
+    }
+
+    public void Play()
+    {
+        score = 0;
+        scoreText.text = score.ToString();
+
+        playButton.SetActive(false);
+        gameOver.SetActive(false);
+
+        Time.timeScale = 1f;
+        player.enabled = true;
+
+        Pipes[] pipes = FindObjectsOfType<Pipes>();
+
+        for (int i = 0; i < pipes.Length; i++)
+        {
+            Destroy(pipes[i].gameObject);
+        }
+    }
+
+    public void Pause()
+    {
+        Time.timeScale = 0f;
+        player.enabled = false;
+    }
     public void GameOver()
     {
-        Debug.Log("Game over");
+        gameOver.SetActive(true);
+        playButton.SetActive(true);
+
+        Pause();
     }
     public void IncreaseScore()
     {
         score ++;
+        scoreText.text = score.ToString();
     }
 }
